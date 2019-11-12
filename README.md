11693d7fa298fb10cd77a527403ad7e4ac012572
galiii <gali.brill.1408@gmail.com>
2019-11-12 12:31:44 +0200

    first step style

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # minesweeper
 Minesweeper
+
+#### Description
+* The bord will 8X8
+* 10 Mines
+* color grey
+* 
+
+
---
+++ index.js
@@ -0,0 +1,20 @@
+
+//let itemsDiv = [];
+
+function addElements(){
+const divCon = document.querySelector('.dashbourd-game');
+for(let i=0;i<81;i++){
+var itemsDiv = document.createElement("div"); // create a new div element
+itemsDiv.setAttribute("class", "box-unswitch");
+itemsDiv.setAttribute("class", "cell");
+console.log(typeof itemsDiv); 
+console.log(itemsDiv);
+let newContent = document.createTextNode(1); // and give it some content 
+itemsDiv.appendChild(newContent); //add the text node to the newly created div 
+divCon.appendChild(itemsDiv);
+}
+
+
+}
+
+addElements();
