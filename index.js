e65358df073c30d9b773896db95835e796b9fcaf
galiii <gali.brill.1408@gmail.com>
2019-11-13 23:31:31 +0200

    stle

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ function addElements(){
 const divCon = document.querySelector('.dashbourd-game');
 for(let i=0;i<81;i++){
 var itemsDiv = document.createElement("div"); // create a new div element
-itemsDiv.setAttribute("class", "box-unswitch");
-itemsDiv.setAttribute("class", "cell");
+//itemsDiv.setAttribute("class", "box-unswitch");
+itemsDiv.setAttribute("class", "cell","number");
+
+//itemsDiv.setAttribute("class", "number");
+//itemsDiv.setAttribute("class", "one");
 console.log(typeof itemsDiv); 
 console.log(itemsDiv);
 let newContent = document.createTextNode(1); // and give it some content 
