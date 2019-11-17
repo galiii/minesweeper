4a9c843475d095f35e7f79853128b744f5e7d728
galiii <gali.brill.1408@gmail.com>
2019-11-17 08:09:35 +0200

    style

--- index.js
+++ index.js
@@ -2,15 +2,11 @@
 //let itemsDiv = [];
 
 function addElements(){
-const divCon = document.querySelector('.dashbourd-game');
+//const divCon = document.querySelector('.dashbourd-game');
+const divCon = document.getElementById('game');
 for(let i=0;i<81;i++){
 var itemsDiv = document.createElement("div"); // create a new div element
-//itemsDiv.setAttribute("class", "box-unswitch");
-itemsDiv.setAttribute("class", "cell","number");
-
-//itemsDiv.setAttribute("class", "number");
-//itemsDiv.setAttribute("class", "one");
-console.log(typeof itemsDiv); 
+itemsDiv.classList.add("cell","number","one","main-color");
 console.log(itemsDiv);
 let newContent = document.createTextNode(1); // and give it some content 
 itemsDiv.appendChild(newContent); //add the text node to the newly created div 
