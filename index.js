f59984ce7dedd8a362bd65270fbbadf115a0b580
galiii <gali.brill.1408@gmail.com>
2019-11-27 09:39:41 +0200

    function of the game

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 
-//let itemsDiv = [];
+
+function Cell(){
+
+
+};
+
+
+class Dashboard{
+
+}
+
+
 
 function addElements(){
 const divCon = document.getElementById('game');
 for(let i=0;i<81;i++){
 var itemsDiv = document.createElement("div"); // create a new div element
-itemsDiv.classList.add("cell","number","one","main-color");
+itemsDiv.classList.add("open","number");
 console.log(itemsDiv);
 let newContent = document.createTextNode(1); // and give it some content 
 itemsDiv.appendChild(newContent); //add the text node to the newly created div 
 divCon.appendChild(itemsDiv);
 }
-
-
 }
+
 
 addElements();
